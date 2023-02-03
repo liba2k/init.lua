2e94ac5dc36d22305d4aaaa4fd6544f6dd5ccfa7
liba2k <liba2k@dont_email_me.com>
2023-02-02 19:42:40 -0800

    Remapping the save to C-a, since C-w is used to move between split buffers.

--- lua/theprimeagen/remap.lua
+++ lua/theprimeagen/remap.lua
@@ -45,4 +45,4 @@ vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 vim.keymap.set("n", "<leader>vpp", "<cmd>e ~/.dotfiles/nvim/.config/nvim/lua/theprimeagen/packer.lua<CR>");
 vim.keymap.set("n", "<leader>mr", "<cmd>CellularAutomaton make_it_rain<CR>");
 
-vim.keymap.set("n", "<C-w>", ":w<cr>");
+vim.keymap.set("n", "<C-a>", ":w<cr>");
