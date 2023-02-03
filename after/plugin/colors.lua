0e29f637d5636805247ccd90dfb35773b4608121
liba2k <liba2k@dont_email_me.com>
2023-02-03 06:57:04 -0800

    Going for Carbon

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -1,9 +1,8 @@
 require('rose-pine').setup({
     disable_background = true
 })
-
 function ColorMyPencils(color)
-	color = color or "nightfox"
+	color = color or "carbonfox"
 	vim.cmd.colorscheme(color)
 
 	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
