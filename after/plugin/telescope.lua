ddbc190f0349636b7998237b30c746d0fd64f859
liba2k <liba2k@gmail.com>
2023-02-28 13:50:23 -0800

    Add key binding for buffers

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,4 +1,5 @@
 local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<C-b>', builtin.buffers, {})
 vim.keymap.set('n', '<C-p>', builtin.find_files, {})
 vim.keymap.set('n', '<C-g>', builtin.git_files, {})
 vim.keymap.set('n', '<C-]>', function()
